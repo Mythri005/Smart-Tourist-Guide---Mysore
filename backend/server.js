26ada60d91a952e6ca7a374393e240a90366f0cd
Mythri005 <mythrikrishna4@gmail.com>
2026-04-07 15:20:38 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,5 @@ app.use((err, req, res, next) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log(`Server running on port ${PORT}`);
 });
